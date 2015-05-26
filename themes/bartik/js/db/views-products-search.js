e45511b4ab36704656afc3e8cfda9cd566919136
Harry Qin <zergling9999@hotmail.com>
2015-05-26 16:07:40 +0800

    show all in product search filter select lists

--- themes/bartik/js/db/views-products-search.js
+++ themes/bartik/js/db/views-products-search.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function($) {
 
-$("select#edit-category option[value='All']").text(Drupal.t('Category'));
-$("select#edit-color option[value='All']").text(Drupal.t('Color'));
-$("select#edit-country option[value='All']").text(Drupal.t('Country'));
-$("select#edit-taste option[value='All']").text(Drupal.t('Taste'));
-$("select#edit-specials option[value='All']").text(Drupal.t('Specials'));
-$("select#edit-pricing option[value='All']").text(Drupal.t('Pricing range'));
+$("select#edit-category option[value='All']").text(Drupal.t('All Category'));
+$("select#edit-color option[value='All']").text(Drupal.t('All Color'));
+$("select#edit-country option[value='All']").text(Drupal.t('All Country'));
+$("select#edit-taste option[value='All']").text(Drupal.t('All Taste'));
+$("select#edit-specials option[value='All']").text(Drupal.t('All Specials'));
+$("select#edit-pricing option[value='All']").text(Drupal.t('All Pricing range'));
 
 $(".views-exposed-widgets select[id*='edit-']").chosen({disable_search_threshold: 10});
 
-$("ul.chzn-results [id*='_0']").css("display", "none");
+// $("ul.chzn-results [id*='_0']").css("display", "none");
 
 });
