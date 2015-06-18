554dfada8299cef6f84b883562508137923666d4
Harry Qin <zergling9999@hotmail.com>
2015-06-18 18:43:59 +0800

    f

--- themes/bartik/templates/block/block--block--5.tpl.php
+++ themes/bartik/templates/block/block--block--5.tpl.php
@@ -4,7 +4,7 @@
   <div id="home-first-content">
     <div class="col col-center">
       <?php
-      $nid = db_query("SELECT nid FROM {node} WHERE status = 1 AND type = 'hmi' AND language = :$language LIMIT 0, 1", array(':$language' => $$language->$language))->fetchField();
+      $nid = db_query("SELECT nid FROM {node} WHERE status = 1 AND type = 'hmi' AND language = :language LIMIT 0, 1", array(':$language' => $language->language))->fetchField();
       if ($nid) {
         $node = node_load($nid);
         $field_hmi_image = field_get_items('node', $node, 'field_hmi_image');
